62e01f6ad0fed23220d00c4ffe312fbcc9b98fa1
Roshan Varghese <roshan.varghese612@gmail.com>
2022-01-26 23:06:53 +0530

    First commit

---
+++ Backend/artforum/src/main/java/com/artforum/artforum/ArtforumApplication.java
@@ -0,0 +1,13 @@
+package com.artforum.artforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtforumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtforumApplication.class, args);
+	}
+
+}
